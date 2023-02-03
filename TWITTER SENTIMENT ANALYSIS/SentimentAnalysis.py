7fd0b245d50550d75e7881b694bc6af26888db07
girirajbidwai <95566572+girirajbidwai@users.noreply.github.com>
2023-02-03 16:52:03 +0530

    Create SentimentAnalysis.py

---
+++ TWITTER SENTIMENT ANALYSIS/SentimentAnalysis.py
@@ -0,0 +1,28 @@
+#Get the subjectivity, polarity and sentiment :
+
+# Create a function to get the subjectifvity
+def getSubjectivity(tweet: str) -> float:
+    return TextBlob(tweet).sentiment.subjectivity
+
+
+# Create a function to get the polarity
+def getPolarity(tweet: str) -> float:
+    return TextBlob(tweet).sentiment.polarity
+
+
+def getSentiment(polarityValue: int) -> str:
+    if polarityValue < 0:
+        return 'Negative'
+    elif polarityValue == 0:
+        return 'Neutral'
+    else:
+        return 'Positive'
+  
+#Apply it to tweet:
+subjectivity = F.udf(getSubjectivity, FloatType())
+polarity = F.udf(getPolarity, FloatType())
+sentiment = F.udf(getSentiment, StringType())
+
+subjectivity_tweets = raw_tweets.withColumn('subjectivity', subjectivity(col("processed_text")))
+polarity_tweets = subjectivity_tweets.withColumn("polarity", polarity(col("processed_text")))
+sentiment_tweets = polarity_tweets.withColumn("sentiment", sentiment(col("polarity")))
