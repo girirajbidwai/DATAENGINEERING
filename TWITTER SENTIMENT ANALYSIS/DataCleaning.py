66af8706fb94ff8a5623887974e4e5fdeb86ed98
girirajbidwai <95566572+girirajbidwai@users.noreply.github.com>
2023-02-03 16:49:37 +0530

    Create DataCleaning.py

---
+++ TWITTER SENTIMENT ANALYSIS/DataCleaning.py
@@ -0,0 +1,29 @@
+
+#The cleaning process is to remove links, users, punctuation, numbers and hashtags.
+
+def cleanTweet(tweet: str) -> str:
+    tweet = re.sub(r'http\S+', '', str(tweet))
+    tweet = re.sub(r'bit.ly/\S+', '', str(tweet))
+    tweet = tweet.strip('[link]')
+
+    # remove users
+    tweet = re.sub('(RT\s@[A-Za-z]+[A-Za-z0-9-_]+)', '', str(tweet))
+    tweet = re.sub('(@[A-Za-z]+[A-Za-z0-9-_]+)', '', str(tweet))
+
+    # remove puntuation
+    my_punctuation = '!"$%&\'()*+,-./:;<=>?[\\]^_`{|}~•@â'
+    tweet = re.sub('[' + my_punctuation + ']+', ' ', str(tweet))
+
+    # remove number
+    tweet = re.sub('([0-9]+)', '', str(tweet))
+
+    # remove hashtag
+    tweet = re.sub('(#[A-Za-z]+[A-Za-z0-9-_]+)', '', str(tweet))
+
+    return tweet
+  
+  
+#Here, we apply the clean function on UDF (User Defined Function) and then create a new column with the clean data.
+df1 = values.select("tweet.*")
+clean_tweets = F.udf(cleanTweet, StringType())
+raw_tweets = df1.withColumn('processed_text', clean_tweets(col("text")))
